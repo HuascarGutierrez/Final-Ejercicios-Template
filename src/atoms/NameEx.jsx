e0e03f2b0331d9243ddfee94f63eed0edf43de69
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2023-11-30 11:18:38 -0400

    atomo imagen

--- src/atoms/NameEx.jsx
+++ src/atoms/NameEx.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
+import "./styles/NameEx.css"
 function NameEx(props) {
   return (
     <div>
-      <p>{props.name}</p>
+      <p className='nombre'>{props.name}</p>
     </div>
   )
 }
